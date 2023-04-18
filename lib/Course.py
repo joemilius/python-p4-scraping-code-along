a8ed7f63d7a12a0b2481ed1e197d84a5fd500ab6
Joe Milius <joemilius@gmail.com>
2023-04-18 15:52:52 -0600

    Completed

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,16 @@
 class Course:
+    def __init__(self, title = '', schedule = '', description = ''):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+        pass
+
+    def __str__(self):
+        output = ''
+        output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '------------------'
+        return output
+        
+
+
     pass
